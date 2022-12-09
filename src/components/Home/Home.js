9de5090eaecdc268787de0607ddc0404c6bb3025
MannanAnxari <mannananxari@gmail.com>
2022-12-09 17:44:10 +0500

    wow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,11 +10,12 @@ export const Home = () => {
         <>
             <div>
                 <div className='bluredHeader position-relative overflow-hidden'>
-                    <div className={`blur position-${config.HEADER_TYPE}`} ></div>
+                    <div className={`blur position-${config.HEADER_TYPE}`} >
                     {/* <div className={`blur position-${config.HEADER_TYPE}`} style={{ "backgroundImage": `url('${Banner}')` }}></div> */}
                     <video width="100%" height="100%" loop muted autoPlay className='homeVideo'>
                         <source type="video/mp4" src={BannerVideo} />
                     </video>
+                    </div>
                     <div className="bannerLogo position-absolute top-50 start-50 translate-middle "><img src={BannerLogo} alt="Banner Logo" /></div>
                 </div>
             </div>
