a27e69bf39fb15146b749f57a8ff064c34c148e4
MannanAnxari <mannananxari@gmail.com>
2022-12-12 17:15:27 +0500

    wow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,18 +13,20 @@ export const Home = () => {
     return (
         <>
             {location.pathname === "/" &&
-                <div className='position-relative'>
-                    <Scroll />
-                    <div className='bluredHeader position-relative overflow-hidden'>
-                        <div className={`blur position-${config.HEADER_TYPE}`} >
-                            {/* <div className={`blur position-${config.HEADER_TYPE}`} style={{ "backgroundImage": `url('${Banner}')` }}></div> */}
-                            <video width="100%" height="100%" loop muted autoPlay className='homeVideo'>
-                                <source type="video/mp4" src={BannerVideo} />
-                            </video>
+                <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} exit={{ transition: { duration: 0.3 }, opacity: 0, x: 100 }} id="home">
+                    <div className='position-relative'>
+                        <Scroll />
+                        <div className='bluredHeader position-relative overflow-hidden'>
+                            <div className={`blur position-${config.HEADER_TYPE}`} >
+                                {/* <div className={`blur position-${config.HEADER_TYPE}`} style={{ "backgroundImage": `url('${Banner}')` }}></div> */}
+                                <video width="100%" height="100%" loop muted autoPlay className='homeVideo'>
+                                    <source type="video/mp4" src={BannerVideo} />
+                                </video>
+                            </div>
+                            <div className="bannerLogo position-absolute top-50 start-50 translate-middle "><img src={BannerLogo} alt="Banner Logo" /></div>
                         </div>
-                        <div className="bannerLogo position-absolute top-50 start-50 translate-middle "><img src={BannerLogo} alt="Banner Logo" /></div>
                     </div>
-                </div>
+                </motion.div>
             }
         </>
     )
