ae421e642c7cc904063dbc565397ad2c44a5d175
naveedullahhere <naveedullahhere@gmail.com>
2023-01-04 15:41:31 +0500

    mobile app

--- src/components/SingleServices/AppDevelpment.js
+++ src/components/SingleServices/AppDevelpment.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import serviceone from "../Assets/mobile-app-dev.png";
+import mobileapp from "../Assets/services/mobile-application-development.jpg";
 import LetsTalk from '../LetsTalk/LetsTalk';
 import { Tech } from '../Technologies/Tech';
 
@@ -28,87 +29,27 @@ export const AppDevelpment = () => {
                 </div>
             </section>
             <section className="section bg-radial">
-                <div className="container">
+                <div className="container-fluide">
+
                     <div className="row ">
                         <div className="col-lg-6 col-12 my-auto">
+                            <img src={mobileapp} alt="About" draggable="false" className=' w-100' />
+                        </div>
+                        <div className="col-lg-6 col-12 p-5 my-auto">
                             <div className="inner">
-                                <div className="head">
-                                    <h4 className='heading-sm'>Our Service</h4>
-                                    <h2 className='heading'>Mobile Application Development<span className="dot">.</span></h2>
+                                <div className="head py-2">
+                                    <h2 className='heading heading-h'>Digital Auxilius – Helping global brands introduce superior digital products<span className="dot">.</span></h2>
+                                    <p className='para'>
+                                        We understand how crucial it is for businesses to keep at pace with the ever-changing consumer behavior. Modern technology is constantly evolving and is also playing its part to influence how people shop and satisfy their needs. Today, people expect services/goods to be as accessible as if at their fingertips – a phenomenon that gave birth to mobile app development.</p>
+                                    <p className="para">
+                                        Using an agile development methodology, Digital Auxilius churns android app development and iOS app development for businesses of all scales. We aim to create solutions that not only meet the user’s needs but also cater to the business industry and its vertical needs. We offer a versatile engagement model that begins with a detailed assessment of your business and IT requirements to deliver a mobile app design that allows quick and cost-effective solutions.</p>
                                 </div>
                             </div>
-                            {/* <Button link="" text="Learn more" type="dark" /> */}
-                        </div>
-                    </div>
-                    <div className="card mb-3 col-md-12 card-css-h">
-                        <div className="row g-0 " >
-                            <div className="col-md-4 card-css-h-img " >
-                                <img src="/assets/services_3.png" className="img-fluid rounded-start " alt="..." />
-
-                            </div>
-                            <div className="col-md-8 my-auto" >
-                                <div className="card-body ps-md-5">
-                                    <h5 className="card-title heading-sm">Part One</h5>
-                                    <h5 className="card-title heading">Part One</h5>
-                                    <p className="card-text para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint porro quod illum fugit libero quos, ducimus ab explicabo mollitia eligendi dicta eius reiciendis nihil, itaque nobis! Ea neque at ut! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium porro ab veniam, architecto quidem labore ex doloribus magnam ut recusandae nobis ipsa esse ipsum deleniti veritatis debitis nesciunt cumque? Reiciendis.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card mb-3 col-md-12 card-css-h">
-                        <div className="row g-0 flex-md-row flex-column-reverse" >
-                            <div className="col-md-8 my-auto" >
-                                <div className="card-body pe-md-5">
-                                    <h5 className="card-title heading-sm">Part Two</h5>
-                                    <h5 className="card-title heading">Part Two</h5>
-                                    <p className="card-text para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint porro quod illum fugit libero quos, ducimus ab explicabo mollitia eligendi dicta eius reiciendis nihil, itaque nobis! Ea neque at ut! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium porro ab veniam, architecto quidem labore ex doloribus magnam ut recusandae nobis ipsa esse ipsum deleniti veritatis debitis nesciunt cumque? Reiciendis.</p>
-
-                                </div>
-                            </div>
-                            <div className="col-md-4 card-css-h-img" >
-                                <img src="/assets/services_3.png" className="img-fluid rounded-start" alt="..." />
-
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="card mb-3 col-md-12 card-css-h">
-                        <div className="row g-0 " >
-                            <div className="col-md-4 card-css-h-img " >
-                                <img src="/assets/services_3.png" className="img-fluid rounded-start " alt="..." />
-
-                            </div>
-                            <div className="col-md-8 my-auto" >
-                                <div className="card-body ps-md-5">
-                                    <h5 className="card-title heading-sm">Part Three</h5>
-                                    <h5 className="card-title heading">Part Three</h5>
-                                    <p className="card-text para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint porro quod illum fugit libero quos, ducimus ab explicabo mollitia eligendi dicta eius reiciendis nihil, itaque nobis! Ea neque at ut! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium porro ab veniam, architecto quidem labore ex doloribus magnam ut recusandae nobis ipsa esse ipsum deleniti veritatis debitis nesciunt cumque? Reiciendis.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card mb-3 col-md-12 card-css-h">
-                        <div className="row g-0 flex-md-row flex-column-reverse" >
-                            <div className="col-md-8 my-auto" >
-                                <div className="card-body pe-md-5">
-                                    <h5 className="card-title heading-sm">Part Four</h5>
-                                    <h5 className="card-title heading">Part Four</h5>
-                                    <p className="card-text para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint porro quod illum fugit libero quos, ducimus ab explicabo mollitia eligendi dicta eius reiciendis nihil, itaque nobis! Ea neque at ut! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium porro ab veniam, architecto quidem labore ex doloribus magnam ut recusandae nobis ipsa esse ipsum deleniti veritatis debitis nesciunt cumque? Reiciendis.</p>
-
-                                </div>
-                            </div>
-                            <div className="col-md-4 card-css-h-img" >
-                                <img src="/assets/services_3.png" className="img-fluid rounded-start" alt="..." />
-
-                            </div>
-
                         </div>
                     </div>
                 </div>
-            </section><br />
-            <section className="section pt-0">
+            </section>
+            <section className="section ">
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
